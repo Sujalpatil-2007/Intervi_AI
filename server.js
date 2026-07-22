29e28814c13dab57a559c0f862328f5522506a4e
Sujalpatil-2007 <patilsujalvr4@gmail.com>
2026-07-22 22:37:49 +0530

    basics

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 require("dotenv").config();
 const express = require("express");
 const app = require("./src/app");
+const connectDB = require("./src/db/db");
 
-app.use(express.json());
+connectDB();
 
 app.listen(3000,()=>{
     console.log("Server is runing on port 3000");

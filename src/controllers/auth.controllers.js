c18ee30659a338b8890e00fb20b128c3417b8ff7
Sujalpatil-2007 <patilsujalvr4@gmail.com>
2026-07-23 23:41:51 +0530

    updates

--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -1,1 +1,12 @@
 const userModel = require("../models/user.model");
+
+/*
+*user register controller
+*POST /api/auth/register
+*/
+
+function userRegisterController(req, res) {
+    
+    const {fullName, email, password, avatar, targetRole, experienceLevel, skills} = req.body;
+
+}

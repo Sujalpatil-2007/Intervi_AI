63e2a5598f96b1df20ba8669a0a6e9f6580776e6
Sujalpatil-2007 <patilsujalvr4@gmail.com>
2026-07-24 14:24:05 +0530

    login api done

--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -30,7 +30,7 @@ async function userRegisterController(req, res) {
   });
 
   if (isExists) {
-    return res.status(422).json({
+    return res.status(409).json({
       success: false,
       message: "User already exists with this Email.",
     });
@@ -80,6 +80,74 @@ async function userRegisterController(req, res) {
 
 }
 
+/**
+ *  - user login controller
+ *  - POST /api/auth/login
+ */
+
+async function userLoginController(req, res) {
+    try {
+        const {email, password} = req.body;
+
+        if (!email || !password) {
+  return res.status(400).json({
+    success: false,
+    message: "Email and password are required.",
+  });
+}
+
+    const user = await userModel.findOne({email}).select("+password");
+
+    if(!user){
+        return res.status(401).json({
+            success:false,
+            message:"Email or password is INVALID"
+        })
+    }
+
+    const isValidPassword = await user.comparePassword(password);
+
+    if(!isValidPassword){
+        return res.status(401).json({
+            success:false,
+            message:"Email or password is INVALID"
+        })
+    }
+
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {expiresIn: "3d"});
+
+    res.cookie("token", token, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: "strict",
+    maxAge: 3 * 24 * 60 * 60 * 1000, // 3 days
+    });
+
+    res.status(200).json({
+    success:true,
+    message:"User Login successfully",
+    user:{
+        _id:user._id,
+        email:user.email,
+        fullName:user.fullName,
+        avatar:user.avatar,
+        targetRole:user.targetRole,
+        experienceLevel:user.experienceLevel,
+        skills:user.skills,
+    },
+    token
+  })
+    } catch (err) {
+        console.error(err);
+
+        return res.status(500).json({
+        success: false,
+        message: "Internal Server Error",
+        });
+    }
+}
+
 module.exports = {
   userRegisterController,
+  userLoginController,
 };

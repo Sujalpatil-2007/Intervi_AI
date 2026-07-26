37d8627ea9566ba3fc6b1495adf8b2a7e2c4ef10
Sujalpatil-2007 <patilsujalvr4@gmail.com>
2026-07-26 23:30:50 +0530

    interview controller completed

---
+++ src/controllers/interview.controller.js
@@ -0,0 +1,62 @@
+const {
+  generateInterview,
+  getInterviewById,
+  getUserInterviews,
+} = require("../service/interview.service");
+
+async function generateInterviewController(req, res, next) {
+  try {
+    const result = await generateInterview({
+      userId: req.user._id,
+      ...req.body,
+    });
+
+    res.status(201).json({
+      success: true,
+      message: "Interview generated successfully.",
+      data: result,
+    });
+  } catch (error) {
+    next(error);
+  }
+}
+
+async function getMyInterviewsController(req,res,next) {
+  try {
+    const result = await getUserInterviews({
+      userId: req.user._id,
+      page: Number(req.query.page || 1),
+      limit: Number(req.query.limit || 10),
+      status: req.query.status,
+    });
+
+    res.json({
+      success: true,
+      ...result,
+    });
+  } catch (error) {
+    next(error);
+  }
+}
+
+async function getInterviewController(req,res,next) {
+  try {
+    const result = await getInterviewById({
+      interviewId: req.params.id,
+      userId: req.user._id,
+    });
+
+    res.json({
+      success: true,
+      data: result,
+    });
+  } catch (error) {
+    next(error);
+  }
+}
+
+module.exports = {
+  getMyInterviewsController,
+  getInterviewController,
+  generateInterviewController,
+}

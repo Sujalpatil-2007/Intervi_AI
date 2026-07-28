7ddeaacdb5fe15fc1efe1ba952dc8783df342629
Sujalpatil-2007 <patilsujalvr4@gmail.com>
2026-07-28 23:21:01 +0530

    recent route added

--- src/routes/dashboard.routes.js
+++ src/routes/dashboard.routes.js
@@ -6,8 +6,15 @@ const protect = require("../middleware/auth.middleware");
 
 const {
   getDashboardSummaryController,
+  getRecentInterviewsController,
 } = require("../controllers/dashboard.controller");
 
 router.get("/summary", protect, getDashboardSummaryController);
+
+router.get(
+  "/recent",
+  protect,
+  getRecentInterviewsController
+);
 
 module.exports = router;

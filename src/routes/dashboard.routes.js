0d9cc51e219f4c40b66e8aaf388de9048c387bbb
Sujalpatil-2007 <patilsujalvr4@gmail.com>
2026-07-30 22:06:41 +0530

    score-trend route added

--- src/routes/dashboard.routes.js
+++ src/routes/dashboard.routes.js
@@ -7,10 +7,13 @@ const protect = require("../middleware/auth.middleware");
 const {
   getDashboardSummaryController,
   getRecentInterviewsController,
+  getScoreTrendController,
 } = require("../controllers/dashboard.controller");
 
 router.get("/summary", protect, getDashboardSummaryController);
 
 router.get("/recent", protect, getRecentInterviewsController);
+
+router.get("/score-trend", protect, getScoreTrendController);
 
 module.exports = router;

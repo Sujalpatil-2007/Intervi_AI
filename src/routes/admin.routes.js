9608e46420e53c9a706f1375599e8eb9d70468c8
Sujalpatil-2007 <patilsujalvr4@gmail.com>
2026-07-31 22:50:28 +0530

    users route added

--- src/routes/admin.routes.js
+++ src/routes/admin.routes.js
@@ -5,8 +5,9 @@ const router = express.Router();
 const protect = require("../middleware/auth.middleware");
 const admin = require("../middleware/admin.middleware");
 
-const { getDashboardController } = require("../controllers/admin.controller");
+const { getDashboardController, getUsersController } = require("../controllers/admin.controller");
 
 router.get("/dashboard", protect, admin, getDashboardController);
+router.get("/users", protect, admin, getUsersController);
 
 module.exports = router;

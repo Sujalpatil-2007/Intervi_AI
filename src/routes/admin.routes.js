35f723171d7eb266cba24a1267fda4f2ed89cc69
Sujalpatil-2007 <patilsujalvr4@gmail.com>
2026-08-01 23:40:32 +0530

    users rotes added

--- src/routes/admin.routes.js
+++ src/routes/admin.routes.js
@@ -5,9 +5,18 @@ const router = express.Router();
 const protect = require("../middleware/auth.middleware");
 const admin = require("../middleware/admin.middleware");
 
-const { getDashboardController, getUsersController } = require("../controllers/admin.controller");
+const {
+  getDashboardController,
+  getUsersController,
+  getUserByIdController,
+  updateUserRoleController,
+  updateUserBlockStatusController,
+} = require("../controllers/admin.controller");
 
 router.get("/dashboard", protect, admin, getDashboardController);
 router.get("/users", protect, admin, getUsersController);
+router.get("/users/:id", protect, admin, getUserByIdController);
+router.patch("/users/:id/role", protect, admin, updateUserRoleController);
+router.patch("/users/:id/block",protect,admin,updateUserBlockStatusController,);
 
 module.exports = router;

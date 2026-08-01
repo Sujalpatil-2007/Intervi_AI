f270cf322a4f559f34d071e8d7345efb1973053a
Sujalpatil-2007 <patilsujalvr4@gmail.com>
2026-08-01 23:39:13 +0530

    getUserByIdController added

--- src/controllers/admin.controller.js
+++ src/controllers/admin.controller.js
@@ -1,4 +1,4 @@
-const { getDashboard, getUsers } = require("../service/admin.service");
+const { getDashboard, getUsers, getUserById, updateUserRole, updateUserBlockStatus} = require("../service/admin.service");
 
 const getDashboardController = async (req, res, next) => {
   try {
@@ -28,7 +28,61 @@ const getUsersController = async (req, res, next) => {
   }
 };
 
+const getUserByIdController = async (req, res, next) => {
+  try {
+    const data = await getUserById(req.params.id);
+
+    res.status(200).json({
+      success: true,
+      message: "User fetched successfully.",
+      data,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+const updateUserRoleController = async (req, res, next) => {
+   try {
+    const data = await updateUserRole(
+      req.params.id,
+      req.body,
+      req.user
+    );
+
+    res.status(200).json({
+      success: true,
+      message: "User role updated successfully.",
+      data,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+const updateUserBlockStatusController = async (req, res, next) => {
+  try {
+    const data = await updateUserBlockStatus(
+      req.params.id,
+      req.body,
+      req.user
+    );
+
+    res.status(200).json({
+      success: true,
+      message: `User ${data.isBlocked ? "blocked" : "unblocked"} successfully.`,
+      data,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+
 module.exports = {
   getDashboardController,
   getUsersController,
+  getUserByIdController,
+  updateUserRoleController,
+  updateUserBlockStatusController,
 };

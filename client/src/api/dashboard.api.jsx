569158eb94b4f773ede6da85e31fed146324e22d
Sujalpatil-2007 <patilsujalvr4@gmail.com>
2026-08-03 23:02:38 +0530

    dashboard apis call

---
+++ client/src/api/dashboard.api.jsx
@@ -0,0 +1,26 @@
+import api from "./axios";
+
+export const getDashboardSummary = async () => {
+  const response = await api.get("/dashboard/summary");
+  return response.data;
+};
+
+export const getRecentActivity = async () => {
+  const response = await api.get("/dashboard/recent");
+  return response.data;
+};
+
+export const getScoreTrend = async () => {
+  const response = await api.get("/dashboard/score-trend");
+  return response.data;
+};
+
+export const getSkillPerformance = async () => {
+  const response = await api.get("/dashboard/skills");
+  return response.data;
+};
+
+export const getLeaderboard = async () => {
+  const response = await api.get("/dashboard/leaderboard");
+  return response.data;
+};

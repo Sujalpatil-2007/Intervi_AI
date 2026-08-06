76e30fd09df66e80b9870f43aef094e99dc984a8
Sujalpatil-2007 <patilsujalvr4@gmail.com>
2026-08-06 22:18:39 +0530

    toster added

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Toaster } from "react-hot-toast";
 
 import App from "./App";
 import "./styles.css";
@@ -11,8 +12,9 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <QueryProvider>
       <AuthProvider>
+        <Toaster position="top-right" reverseOrder={false} />
         <App />
       </AuthProvider>
     </QueryProvider>
-  </React.StrictMode>
+  </React.StrictMode>,
 );

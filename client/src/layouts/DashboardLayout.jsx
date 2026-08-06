0da806cc7d2022595ee29ff6069fd0a354ee8830
Sujalpatil-2007 <patilsujalvr4@gmail.com>
2026-08-06 16:06:16 +0530

    updated

--- client/src/layouts/DashboardLayout.jsx
+++ client/src/layouts/DashboardLayout.jsx
@@ -1,13 +1,33 @@
+import { useState } from "react";
 import { Outlet } from "react-router-dom";
 
-function DashboardLayout() {
-  return (
-    <div className="flex min-h-screen">
-      {/* Sidebar */}
+import DashboardNavbar from "../components/layout/DashboardNavbar";
+import DashboardSidebar from "../components/layout/DashboardSidebar";
 
-      <main className="flex-1">
-        <Outlet />
-      </main>
+function DashboardLayout() {
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+
+  const openSidebar = () => {
+    setSidebarOpen(true);
+  };
+
+  const closeSidebar = () => {
+    setSidebarOpen(false);
+  };
+
+  return (
+    <div className="min-h-screen bg-slate-100">
+      {/* Sidebar */}
+      <DashboardSidebar isOpen={sidebarOpen} onClose={closeSidebar} />
+
+      {/* Main Content */}
+      <div className="lg:ml-72">
+        <DashboardNavbar onMenuClick={openSidebar} />
+
+        <main className="min-h-[calc(100vh-64px)] p-6">
+          <Outlet />
+        </main>
+      </div>
     </div>
   );
 }

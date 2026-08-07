59b516e176839c8c634ab7bf82fa36b164e0fd2c
Sujalpatil-2007 <patilsujalvr4@gmail.com>
2026-08-07 21:07:41 +0530

    useDeleteResume created

--- client/src/hooks/mutations/useDeleteResume.jsx
+++ client/src/hooks/mutations/useDeleteResume.jsx
@@ -1,4 +1,5 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
+import toast from "react-hot-toast";
 
 import { deleteResume } from "../../api/resume.api";
 import { QUERY_KEYS } from "../../utils/queryKeys";
@@ -9,10 +10,23 @@ export function useDeleteResume() {
   return useMutation({
     mutationFn: deleteResume,
 
-    onSuccess: () => {
+    onSuccess: (data) => {
+      queryClient.invalidateQueries({
+        queryKey: QUERY_KEYS.MY_RESUME,
+      });
+
       queryClient.invalidateQueries({
         queryKey: QUERY_KEYS.RESUMES,
       });
+
+      toast.success(data?.message || "Resume deleted successfully.");
+    },
+
+    onError: (error) => {
+      const message =
+        error?.response?.data?.message || "Failed to delete resume.";
+
+      toast.error(message);
     },
   });
 }

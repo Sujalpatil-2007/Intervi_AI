1bc7428f2cced2de5b0cd95d8c72867241bf8a7c
Sujalpatil-2007 <patilsujalvr4@gmail.com>
2026-08-10 23:30:45 +0530

    updated

--- client/src/api/interview.api.jsx
+++ client/src/api/interview.api.jsx
@@ -2,40 +2,59 @@ import api from "./axios";
 
 export const generateInterview = async (data) => {
   const response = await api.post("/interview/generate", data);
+
   return response.data;
 };
 
-export const getInterviews = async () => {
-  const response = await api.get("/interview");
+export const getInterviews = async ({ page = 1, limit = 10, status } = {}) => {
+  const params = {
+    page,
+    limit,
+  };
+
+  if (status) {
+    params.status = status;
+  }
+
+  const response = await api.get("/interview", {
+    params,
+  });
+
   return response.data;
 };
 
 export const getInterviewById = async (id) => {
   const response = await api.get(`/interview/${id}`);
+
   return response.data;
 };
 
 export const startInterview = async (id) => {
   const response = await api.post(`/interview/${id}/start`);
+
   return response.data;
 };
 
 export const submitAnswer = async (id, data) => {
   const response = await api.post(`/interview/${id}/answer`, data);
+
   return response.data;
 };
 
 export const finishInterview = async (id) => {
   const response = await api.post(`/interview/${id}/finish`);
+
   return response.data;
 };
 
 export const evaluateInterview = async (id) => {
   const response = await api.post(`/interview/${id}/evaluate`);
+
   return response.data;
 };
 
 export const getInterviewEvaluation = async (id) => {
   const response = await api.get(`/interview/${id}/evaluation`);
+
   return response.data;
 };

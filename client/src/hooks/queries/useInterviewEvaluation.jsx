cbdd2a5ac240c728728e957b442ea0ea7dcb28af
Sujalpatil-2007 <patilsujalvr4@gmail.com>
2026-08-13 23:02:02 +0530

    updated

--- client/src/hooks/queries/useInterviewEvaluation.jsx
+++ client/src/hooks/queries/useInterviewEvaluation.jsx
@@ -3,11 +3,11 @@ import { useQuery } from "@tanstack/react-query";
 import { getInterviewEvaluation } from "../../api/interview.api";
 import { QUERY_KEYS } from "../../utils/queryKeys";
 
-export function useInterviewEvaluation(id) {
+export function useInterviewEvaluation(interviewId) {
   return useQuery({
-    queryKey: QUERY_KEYS.INTERVIEW_EVALUATION(id),
-    queryFn: () => getInterviewEvaluation(id),
-    enabled: Boolean(id),
+    queryKey: QUERY_KEYS.INTERVIEW_EVALUATION(interviewId),
+    queryFn: () => getInterviewEvaluation(interviewId),
+    enabled: Boolean(interviewId),
     staleTime: 5 * 60 * 1000,
   });
-};
+}

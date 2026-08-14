4bc7bce7e7edf05df6964603228e5ac22452ae19
Sujalpatil-2007 <patilsujalvr4@gmail.com>
2026-08-14 22:32:46 +0530

    updated

--- client/src/hooks/mutations/useFinishInterview.jsx
+++ client/src/hooks/mutations/useFinishInterview.jsx
@@ -4,13 +4,13 @@ import toast from "react-hot-toast";
 import { finishInterview } from "../../api/interview.api";
 import { QUERY_KEYS } from "../../utils/queryKeys";
 
-export function useFinishInterview() {
+export function useFinishInterview(interviewId) {
   const queryClient = useQueryClient();
 
   return useMutation({
-    mutationFn: finishInterview,
+    mutationFn: () => finishInterview(interviewId),
 
-    onSuccess: (response, interviewId) => {
+    onSuccess: (response) => {
       queryClient.invalidateQueries({
         queryKey: QUERY_KEYS.INTERVIEW_DETAILS(interviewId),
       });
@@ -19,17 +19,13 @@ export function useFinishInterview() {
         queryKey: QUERY_KEYS.INTERVIEWS,
       });
 
-      toast.success(
-        response?.message ||
-          "Interview completed successfully.",
-      );
+      toast.success(response?.message || "Interview completed successfully.");
     },
 
     onError: (error) => {
       toast.error(
-        error?.response?.data?.message ||
-          "Unable to finish the interview.",
+        error?.response?.data?.message || "Failed to finish the interview.",
       );
     },
   });
-};
+}

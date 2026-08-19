bea122abff69d24b3f160134c3f7fa27dcb15a2b
Sujalpatil-2007 <patilsujalvr4@gmail.com>
2026-08-19 22:25:54 +0530

    updated

--- client/src/hooks/queries/useRecentInterviews.jsx
+++ client/src/hooks/queries/useRecentInterviews.jsx
@@ -1,12 +1,12 @@
 import { useQuery } from "@tanstack/react-query";
 
-import { getRecentInterviews } from "../../api/dashboard.api";
+import { getRecentActivity } from "../../api/dashboard.api";
 import { QUERY_KEYS } from "../../utils/queryKeys";
 
 export function useRecentInterviews() {
   return useQuery({
-    queryKey: QUERY_KEYS.RECENT_INTERVIEWS,
-    queryFn: getRecentInterviews,
+    queryKey: QUERY_KEYS.DASHBOARD_RECENT,
+    queryFn: getRecentActivity,
     staleTime: 5 * 60 * 1000,
   });
 }

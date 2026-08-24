0a774f8c0c3a5227fa8a086a68aff0dd89e535f2
Sujalpatil-2007 <patilsujalvr4@gmail.com>
2026-08-24 23:26:20 +0530

    updated

--- client/src/api/resume.api.jsx
+++ client/src/api/resume.api.jsx
@@ -1,24 +1,26 @@
 import axiosClient from "./axios";
 
 export async function uploadResume(formData, onUploadProgress) {
-  const response = await axiosClient.post("/resume/upload", formData, {
-    headers: {
-      "Content-Type": "multipart/form-data",
-    },
-    onUploadProgress,
-  });
+  console.log("API FormData:", formData);
+  console.log("API resume file:", formData.get("resume"));
+
+  const response = await axiosClient.post(
+    "/resume/upload",
+    formData,
+    {
+      onUploadProgress,
+    }
+  );
 
   return response.data;
 }
 
 export async function getMyResume() {
   const response = await axiosClient.get("/resume/me");
-
   return response.data;
 }
 
 export async function deleteResume() {
   const response = await axiosClient.delete("/resume/delete");
-
   return response.data;
 }

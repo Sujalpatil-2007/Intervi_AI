54900f5df05f56ed47fb2a0cfae86e5fefaacb91
Sujalpatil-2007 <patilsujalvr4@gmail.com>
2026-08-27 22:17:41 +0530

    updated

--- client/src/api/auth.api.jsx
+++ client/src/api/auth.api.jsx
@@ -19,3 +19,9 @@ export const getCurrentUser = async () => {
   const response = await api.get("/auth/me");
   return response.data;
 };
+
+export const updateProfile = async (data) => {
+  const response = await api.put("/auth/profile", data);
+
+  return response.data;
+};

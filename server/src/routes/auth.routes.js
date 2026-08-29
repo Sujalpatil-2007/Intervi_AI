b11912e59f62db9b35c2312146d751daf5036c00
Sujalpatil-2007 <patilsujalvr4@gmail.com>
2026-08-29 23:05:13 +0530

    updated

--- server/src/routes/auth.routes.js
+++ server/src/routes/auth.routes.js
@@ -16,4 +16,7 @@ router.post("/logout",protect,authController.userLogoutController);
 /* GET /api/auth/me */
 router.get("/me",protect,authController.getCurrentUserController);
 
+// PUT /api/auth/profile
+router.put("/profile", protect, authController.updateProfileController);
+
 module.exports = router;

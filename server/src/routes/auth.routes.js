068eb84055c70538a840945e49e61060c75d2927
Sujalpatil-2007 <patilsujalvr4@gmail.com>
2026-08-29 23:05:27 +0530

    updated

--- server/src/routes/auth.routes.js
+++ server/src/routes/auth.routes.js
@@ -5,16 +5,16 @@ const protect = require("../middleware/auth.middleware");
 const router = express.Router();
 
 /* POST /api/auth/register */
-router.post("/register",authController.userRegisterController);
- 
+router.post("/register", authController.userRegisterController);
+
 /* POST /api/auth/login */
-router.post("/login",authController.userLoginController);
+router.post("/login", authController.userLoginController);
 
 /* POST /api/auth/logout */
-router.post("/logout",protect,authController.userLogoutController);
+router.post("/logout", protect, authController.userLogoutController);
 
 /* GET /api/auth/me */
-router.get("/me",protect,authController.getCurrentUserController);
+router.get("/me", protect, authController.getCurrentUserController);
 
 // PUT /api/auth/profile
 router.put("/profile", protect, authController.updateProfileController);

2cade3defd32e41ad524dd1fa3e5906f07189138
Sujalpatil-2007 <patilsujalvr4@gmail.com>
2026-08-29 23:03:27 +0530

    updated

--- server/src/controllers/auth.controllers.js
+++ server/src/controllers/auth.controllers.js
@@ -7,77 +7,78 @@ const jwt = require("jsonwebtoken");
  */
 
 async function userRegisterController(req, res) {
-  try{
+  try {
     const {
-    fullName,
-    email,
-    password,
-    avatar,
-    targetRole,
-    experienceLevel,
-    skills,
-  } = req.body;
+      fullName,
+      email,
+      password,
+      avatar,
+      targetRole,
+      experienceLevel,
+      skills,
+    } = req.body;
 
-  if (!fullName || !email || !password) {
-  return res.status(400).json({
-    success: false,
-    message: "Full name, email and password are required.",
-  });
-}
+    if (!fullName || !email || !password) {
+      return res.status(400).json({
+        success: false,
+        message: "Full name, email and password are required.",
+      });
+    }
 
-  const isExists = await userModel.findOne({
-    email: email,
-  });
-
-  if (isExists) {
-    return res.status(409).json({
-      success: false,
-      message: "User already exists with this Email.",
+    const isExists = await userModel.findOne({
+      email: email,
     });
-  }
 
-  const user = await userModel.create({
-    fullName,
-    email,
-    password,
-    avatar,
-    targetRole,
-    experienceLevel,
-    skills,
-  });
+    if (isExists) {
+      return res.status(409).json({
+        success: false,
+        message: "User already exists with this Email.",
+      });
+    }
 
-  const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {expiresIn: "3d"});
+    const user = await userModel.create({
+      fullName,
+      email,
+      password,
+      avatar,
+      targetRole,
+      experienceLevel,
+      skills,
+    });
 
-  res.cookie("token", token, {
-  httpOnly: true,
-  secure: process.env.NODE_ENV === "production",
-  sameSite: "strict",
-  maxAge: 3 * 24 * 60 * 60 * 1000, // 3 days
-});
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "3d",
+    });
 
-  res.status(201).json({
-    success:true,
-    message:"User registered successfully",
-    user:{
-        _id:user._id,
-        email:user.email,
-        fullName:user.fullName,
-        avatar:user.avatar,
-        targetRole:user.targetRole,
-        experienceLevel:user.experienceLevel,
-        skills:user.skills,
-    },
-    token
-  })
-}catch(err){
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: "strict",
+      maxAge: 3 * 24 * 60 * 60 * 1000, // 3 days
+    });
+
+    res.status(201).json({
+      success: true,
+      message: "User registered successfully",
+      user: {
+        _id: user._id,
+        email: user.email,
+        fullName: user.fullName,
+        avatar: user.avatar,
+        targetRole: user.targetRole,
+        experienceLevel: user.experienceLevel,
+        skills: user.skills,
+      },
+      token,
+    });
+  } catch (err) {
     console.error(err);
 
     return res.status(500).json({
       success: false,
       message: "Internal Server Error",
     });
-}
-
+  }
 }
 
 /**
@@ -86,65 +87,67 @@ async function userRegisterController(req, res) {
  */
 
 async function userLoginController(req, res) {
-    try {
-        const {email, password} = req.body;
+  try {
+    const { email, password } = req.body;
 
-        if (!email || !password) {
-  return res.status(400).json({
-    success: false,
-    message: "Email and password are required.",
-  });
-}
+    if (!email || !password) {
+      return res.status(400).json({
+        success: false,
+        message: "Email and password are required.",
+      });
+    }
 
-    const user = await userModel.findOne({email}).select("+password");
+    const user = await userModel.findOne({ email }).select("+password");
 
-    if(!user){
-        return res.status(401).json({
-            success:false,
-            message:"Email or password is INVALID"
-        })
+    if (!user) {
+      return res.status(401).json({
+        success: false,
+        message: "Email or password is INVALID",
+      });
     }
 
     const isValidPassword = await user.comparePassword(password);
 
-    if(!isValidPassword){
-        return res.status(401).json({
-            success:false,
-            message:"Email or password is INVALID"
-        })
+    if (!isValidPassword) {
+      return res.status(401).json({
+        success: false,
+        message: "Email or password is INVALID",
+      });
     }
 
-    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {expiresIn: "3d"});
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "3d",
+    });
 
     res.cookie("token", token, {
-    httpOnly: true,
-    secure: process.env.NODE_ENV === "production",
-    sameSite: "strict",
-    maxAge: 3 * 24 * 60 * 60 * 1000, // 3 days
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: "strict",
+      maxAge: 3 * 24 * 60 * 60 * 1000, // 3 days
     });
 
     res.status(200).json({
-    success:true,
-    message:"User Login successfully",
-    user:{
-        _id:user._id,
-        email:user.email,
-        fullName:user.fullName,
-        avatar:user.avatar,
-        targetRole:user.targetRole,
-        experienceLevel:user.experienceLevel,
-        skills:user.skills,
-    },
-    token
-  })
-    } catch (err) {
-        console.error(err);
+      success: true,
+      message: "User Login successfully",
+      user: {
+        _id: user._id,
+        email: user.email,
+        fullName: user.fullName,
+        avatar: user.avatar,
+        targetRole: user.targetRole,
+        experienceLevel: user.experienceLevel,
+        skills: user.skills,
+      },
+      token,
+    });
+  } catch (err) {
+    console.error(err);
 
-        return res.status(500).json({
-        success: false,
-        message: "Internal Server Error",
-        });
-    }
+    return res.status(500).json({
+      success: false,
+      message: "Internal Server Error",
+    });
+  }
 }
 
 /**

3ecda7637b56e3db7ce2bea6d3efceacc18b146b
Sujalpatil-2007 <patilsujalvr4@gmail.com>
2026-09-04 23:26:29 +0530

    updated

---
+++ client/src/hooks/queries/useAdminUser.jsx
@@ -0,0 +1,13 @@
+import { useQuery } from "@tanstack/react-query";
+import { getAdminUserById } from "../../api/admin.api";
+
+export const useAdminUser = (id) => {
+  return useQuery({
+    queryKey: ["admin-user", id],
+    queryFn: () => getAdminUserById(id),
+    enabled: Boolean(id),
+  });
+};
+
+
+
